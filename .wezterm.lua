edf2ba020d7b0472a2836b89af902b0dea33b144
tamago3keran <yamataku3831@gmail.com>
2025-01-06 13:37:29 +0000

    update .wezterm.lua for tabs

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,5 +5,7 @@ config.window_background_image = '/Users/takuma/.wezterm/background_image.jpeg'
 config.window_background_image_hsb = {
   brightness = 0.025,
 }
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
 
 return config
